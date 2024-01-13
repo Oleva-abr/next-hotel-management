af2e9d217f90c8040c9adbf3cd07da2fcacb44d6
Olena <72007221+Oleva-abr@users.noreply.github.com>
2024-01-14 00:00:50 +0200

    add news letter

--- src/app/(web)/page.tsx
+++ src/app/(web)/page.tsx
@@ -1,14 +1,16 @@
+import Gallery from "@/components/Gallery/Gallery";
 import HeroSection from "@/components/HeroSection/HeroSection";
 import PageSearch from "@/components/PageSearch/PageSearch";
+import NewsLetter from "@/components/NewsLetter/NewsLetter";
 
 const Home = () => {
   return (
     <>
       <HeroSection />
       <PageSearch />
-      {/* // featured room
-       // Gallery 
-       // News letter */}
+      {/* featured room */}
+      <Gallery />
+      <NewsLetter />
     </>
   );
 };
