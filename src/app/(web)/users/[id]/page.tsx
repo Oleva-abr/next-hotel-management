b589e77c1b10deebcb38080d19e40e239bb4c8f9
Olena <72007221+Oleva-abr@users.noreply.github.com>
2024-01-26 21:32:04 +0200

    create Table component

--- src/app/(web)/users/[id]/page.tsx
+++ src/app/(web)/users/[id]/page.tsx
@@ -6,11 +6,23 @@ import axios from "axios";
 import Image from "next/image";
 import useSWR from "swr";
 import LoadingSpinner from "../../loading";
+import { FaSignOutAlt } from "react-icons/fa";
+import { signOut } from "next-auth/react";
+import { BsJournalBookmarkFill } from "react-icons/bs";
+import { useState } from "react";
+import { GiMoneyStack } from "react-icons/gi";
+import Table from "@/components/Table/Table";
 
 const UserDetails = (props: { params: { id: string } }) => {
   const {
     params: { id: userId },
   } = props;
+
+  const [currentNav, setCurrentNav] = useState<
+    "bookings" | "amount" | "raitings"
+  >("bookings");
+
+  const [roomId, setRoomId] = useState<string | null>(null);
 
   const fetchUserBooking = async () => getUserBookings(userId);
   const fetchUserData = async () => {
@@ -53,6 +65,101 @@ const UserDetails = (props: { params: { id: string } }) => {
               className="img scale-animation rounded-full"
             />
           </div>
+          <div className="font-normal py-4 text-left">
+            <h6 className="text-xl font-bold pb-3">About</h6>
+            <p className="text-sm">{userData.about ?? ""}</p>
+          </div>
+          <div className="font-normal text-left">
+            <h6 className="text-xl font-bold pb-3">{userData.name}</h6>
+          </div>
+          <div className="flex items-center">
+            <p className="mr-2">Sign Out</p>
+            <FaSignOutAlt
+              className="text-3xl cursor-pointer ml-2"
+              onClick={() => signOut({ callbackUrl: "/" })}
+            />
+          </div>
+        </div>
+        <div className="md:col-span-8 lg:col-span-9">
+          <div className="flex items-center">
+            <h5 className="text-2xl font-bold mr-3"> Hello, {userData.name}</h5>
+          </div>
+          <div className="md:hidden w-14 h-14 rounded-full overflow-hidden">
+            <Image
+              className="img scale-animation"
+              width={56}
+              height={56}
+              src={userData.image}
+              alt="User Name"
+            />
+          </div>
+          <p className="block w-fit md:hidden text-sm py-2">
+            {userData.about ?? ""}
+          </p>
+          <p className="text-xs py-2 font-medium">
+            Joined in {userData._createdAt.split("T")[0]}
+          </p>
+          <div className="md:hidden flex items-center my-2">
+            <p className="mr-2">Sign Out</p>
+            <FaSignOutAlt
+              className="text-3xl cursor-pointer ml-2"
+              onClick={() => signOut({ callbackUrl: "/" })}
+            />
+          </div>
+          <nav className="sticky top-0 px-2 w-fit mx-auto md:w-full md:px-5 py-3 mb-8 text-gray-700 border border-gray-200 rounded-lg bg-gray-50 mt-7">
+            <ol
+              className={`${
+                currentNav === "bookings" ? "text-blue-600" : "text-gry-700"
+              } inline-flex mr-1 md:mr-5 item-center space-x-1 md:space-x-3`}
+            >
+              <li
+                onClick={() => setCurrentNav("bookings")}
+                className="inline-flex items-center cursor-pointer"
+              >
+                <BsJournalBookmarkFill />
+                <a className="inline-flex items-center mx-1 md:mx-2 text-xs md:text-sm font-medium">
+                  Current Bookings
+                </a>
+              </li>
+            </ol>
+            <ol
+              className={`${
+                currentNav === "amount" ? "text-blue-600" : "text-gry-700"
+              } inline-flex mr-1 md:mr-5 item-center space-x-1 md:space-x-3`}
+            >
+              <li
+                onClick={() => setCurrentNav("amount")}
+                className="inline-flex items-center cursor-pointer"
+              >
+                <GiMoneyStack />
+                <a className="inline-flex items-center mx-1 md:mx-2 text-xs md:text-sm font-medium">
+                  Amount Spent
+                </a>
+              </li>
+            </ol>
+            {/* <ol
+              className={`${
+                currentNav === "raitings" ? "text-blue-600" : "text-gry-700"
+              } inline-flex mr-1 md:mr-5 item-center space-x-1 md:space-x-3`}
+            >
+              <li
+                onClick={() => setCurrentNav("raitings")}
+                className="inline-flex items-center cursor-pointer"
+              >
+                <BsJournalBookmarkFill />
+                <a className="inline-flex items-center mx-1 md:mx-2 text-xs md:text-sm font-medium">
+                  Current Bookings
+                </a>
+              </li>
+            </ol> */}
+          </nav>
+          {currentNav === "bookings" ? (
+            userBookings && (
+              <Table bookingDetails={userBookings} setRoomId={setRoomId} />
+            )
+          ) : (
+            <></>
+          )}
         </div>
       </div>
     </div>
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,6 @@
 export { default } from "next-auth/middleware"
 
-// export const config = {
-//     matcher: ['/users/:path*', '/api/:path*']
-// }
-
-
 export const config = {
-    matcher: ['/users/:path*']
+    matcher: ['/users/:path*', '/api/:path*']
 }
+
