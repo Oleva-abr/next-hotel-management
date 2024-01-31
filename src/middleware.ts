b05c59bea0883815b6a6cdb13d54338f2b9a6db7
Olena <72007221+Oleva-abr@users.noreply.github.com>
2024-01-31 15:47:00 +0200

    fix middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 export { default } from "next-auth/middleware"
 
-export const config = {
-    matcher: ['/users/:path*', '/api/:path*']
-}
 
+export const config = {
+    matcher: ['/users/:path*']
+}
